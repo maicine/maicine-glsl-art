167b9a91fcea401d614a06725765937fc1668a13
maicine <healthya92@gmail.com>
2019-01-19 04:53:00 +0000

    add target-animation.cpp

---
+++ the-book-of-shader/target-animation.cpp
@@ -0,0 +1,20 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec2 center = vec2(0.5);
+    float r = abs(sin(u_time) * 0.3);
+    float d = distance(center, st);
+    d *= 30.0;
+    d = abs(sin(d) * 0.4);
+    
+    float circle = step(r, d);
+    
+    gl_FragColor = vec4(vec3(circle),1.0);
+}
