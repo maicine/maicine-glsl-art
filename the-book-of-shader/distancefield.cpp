18284d3d84358100949ad5562ffad02c7fc6d55a
maicine <healthya92@gmail.com>
2019-01-19 04:25:32 +0000

    first commit

---
+++ the-book-of-shader/distancefield.cpp
@@ -0,0 +1,15 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec2 center = vec2(0.5);
+    float d = distance(center, st);
+    
+    gl_FragColor = vec4(vec3(d),1.0);
+}
